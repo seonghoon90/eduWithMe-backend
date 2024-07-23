495d4c1b500c1c5c14d2740977cc554ce097845e
jiwoo <1114jiwoo@gmail.com>
2024-07-23 12:14:55 +0900

    [#7] ✨ feat : 이메일 불일치 에러코드 추가

--- src/main/java/com/sparta/eduwithme/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/eduwithme/common/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
 
     // user
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 유저를 찾을 수 없습니다."),
+    EMAIL_MISMATCH(HttpStatus.NOT_FOUND, "이메일이 일치하지 않습니다."),
 
     // room
     SAME_NEW_ROOM_NAME(HttpStatus.BAD_REQUEST, "동일한 이름의 방을 생성할 수 없습니다."),
