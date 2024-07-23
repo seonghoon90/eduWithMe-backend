dd666b86694001fd779f4a000646f7aeb3cd3486
leesewon <leesw1945@gmail.com>
2024-07-23 14:51:57 +0900

    [#4] ✨ feat : 회원가입, 로그인 기능 구현

--- src/main/java/com/sparta/eduwithme/domain/user/UserService.java
+++ src/main/java/com/sparta/eduwithme/domain/user/UserService.java
@@ -1,4 +1,37 @@
 package com.sparta.eduwithme.domain.user;
 
+import com.sparta.eduwithme.common.exception.CustomException;
+import com.sparta.eduwithme.common.exception.ErrorCode;
+import com.sparta.eduwithme.domain.user.dto.SignupRequestDto;
+import com.sparta.eduwithme.domain.user.entity.User;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(SignupRequestDto requestDto) {
+        String email = requestDto.getEmail();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        String nickName = requestDto.getNickName();
+
+        // 중복 체크 있으면 => 이미 등록된 회원
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            throw new CustomException(ErrorCode.USER_NOT_UNIQUE);
+        }
+
+        // 회원가입한 user DB에 저장
+        userRepository.save(new User(email, password, nickName));
+    }
 }
+
---
+++ src/main/java/com/sparta/eduwithme/security/UserDetailsImpl.java
@@ -0,0 +1,35 @@
+package com.sparta.eduwithme.security;
+
+import com.sparta.eduwithme.domain.user.entity.User;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user){
+        this.user = user;
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
