c51eee4fc3f980e3d1db61e5bb1489beef27c511
junbin <ghdwnsqls0@naver.com>
2024-07-24 12:12:32 +0900

    [#35] ✨ fix : Swagger 기능 오류 수정

--- src/main/java/com/sparta/eduwithme/config/SwaggerConfig.java
+++ src/main/java/com/sparta/eduwithme/config/SwaggerConfig.java
@@ -15,8 +15,9 @@ public class SwaggerConfig {
         String jwt = "JWT";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
         Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
+                .name("AccessToken")
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
                 .scheme("bearer")
                 .bearerFormat("JWT")
         );
