ebdc30dee42fc9e5c5e7b7bf967454f72ab59aac
hoon <dotofi@naver.com>
2024-07-24 15:12:02 +0900

    [#40] ✨ feat : Question 단건 조회 기능 구현

--- src/main/java/com/sparta/eduwithme/domain/question/QuestionController.java
+++ src/main/java/com/sparta/eduwithme/domain/question/QuestionController.java
@@ -2,11 +2,7 @@ package com.sparta.eduwithme.domain.question;
 
 import com.sparta.eduwithme.common.response.DataCommonResponse;
 import com.sparta.eduwithme.common.response.StatusCommonResponse;
-import com.sparta.eduwithme.domain.question.dto.QuestionRequestDto;
-import com.sparta.eduwithme.domain.question.dto.QuestionResponseDto;
-import com.sparta.eduwithme.domain.question.dto.QuestionTitleDto;
-import com.sparta.eduwithme.domain.question.dto.QuestionUpdateRequestDto;
-import jakarta.validation.Valid;
+import com.sparta.eduwithme.domain.question.dto.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -48,7 +44,7 @@ public class QuestionController {
                                                                                             @RequestParam String keyword,
                                                                                             @RequestParam(defaultValue = "0") int page) {
         List<QuestionTitleDto> responseDtoList = questionService.searchQuestionByTitle(roomId, keyword, page, PAGE_SIZE);
-        DataCommonResponse<List<QuestionTitleDto>> response = new DataCommonResponse<>(200,"문제 검색을 성공 하였습니다.", responseDtoList);
+        DataCommonResponse<List<QuestionTitleDto>> response = new DataCommonResponse<>(200, "문제 검색을 성공 하였습니다.", responseDtoList);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
@@ -57,8 +53,8 @@ public class QuestionController {
     public ResponseEntity<DataCommonResponse<QuestionResponseDto>> updateQuestion(@PathVariable Long roomId,
                                                                                   @PathVariable Long questionId,
                                                                                   @RequestBody QuestionUpdateRequestDto requestDto) {
-        QuestionResponseDto responseDto = questionService.updateQuestion(roomId, questionId,requestDto);
-        DataCommonResponse<QuestionResponseDto> response = new DataCommonResponse<>(200,"문제 수정이 완료되었습니다.", responseDto);
+        QuestionResponseDto responseDto = questionService.updateQuestion(roomId, questionId, requestDto);
+        DataCommonResponse<QuestionResponseDto> response = new DataCommonResponse<>(200, "문제 수정이 완료되었습니다.", responseDto);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
@@ -70,4 +66,14 @@ public class QuestionController {
         StatusCommonResponse response = new StatusCommonResponse(204, "문제 삭제가 완료되었습니다.");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    //Question 단건 조회
+    @GetMapping("/rooms/{roomId}/question/{questionId}")
+    public ResponseEntity<DataCommonResponse<QuestionDetailDto>> getQuestion(@PathVariable Long roomId,
+                                                                             @PathVariable Long questionId) {
+        QuestionDetailDto questionDetail = questionService.getQuestionDetail(roomId, questionId);
+        DataCommonResponse<QuestionDetailDto> response = new DataCommonResponse<>(200, "문제 조회에 성공했습니다.", questionDetail);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
