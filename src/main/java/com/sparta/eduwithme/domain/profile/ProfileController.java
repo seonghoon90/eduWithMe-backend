9e6e9d5a6e3f413748a7d92a91fa71ee5e4cf292
jiwoo <1114jiwoo@gmail.com>
2024-07-24 10:38:49 +0900

    [#26] 🩹 fix : 닉네임, 비밀번호 수정 기능 토큰 사용

--- src/main/java/com/sparta/eduwithme/domain/profile/ProfileController.java
+++ src/main/java/com/sparta/eduwithme/domain/profile/ProfileController.java
@@ -1,11 +1,14 @@
 package com.sparta.eduwithme.domain.profile;
 
-import com.sparta.eduwithme.domain.profile.dto.UpdateNicknameRequestDto;
-import com.sparta.eduwithme.domain.profile.dto.UpdatePasswordRequestDto;
-import com.sparta.eduwithme.domain.profile.dto.UserProfileDto;
+import com.sparta.eduwithme.domain.profile.dto.*;
+import com.sparta.eduwithme.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -15,23 +18,31 @@ public class ProfileController {
 
     private final ProfileService profileService;
 
-    @GetMapping("/{userId}")
-    public UserProfileDto getUserProfile(@PathVariable Long userId) {
-        return profileService.getUserProfile(userId);
+    // 프로필 조회
+    @GetMapping
+    public ResponseEntity<UserProfileDto> getUserProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUser().getId();
+        UserProfileDto userProfile = profileService.getUserProfile(userId);
+        return new ResponseEntity<>(userProfile, HttpStatus.OK);
     }
 
-    @PutMapping("/{userId}")
-    public ResponseEntity<String> updateProfile(@PathVariable Long userId,
-                                                @RequestBody UpdateNicknameRequestDto request) {
-        profileService.updateUserProfile(userId, request.getEmail(), request.getNickName());
-
-        return new ResponseEntity<>("프로필 수정이 완료되었습니다.", HttpStatus.OK);
+    // 닉네임 수정
+    @PutMapping
+    public ResponseEntity<UpdateNicknameResponseDto> updateNickname(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                                    @RequestBody UpdateNicknameRequestDto requestDto) {
+        Long userId = userDetails.getUser().getId();
+        profileService.updateUserProfile(userId, requestDto.getEmail(), requestDto.getNickName());
+        UpdateNicknameResponseDto responseDto = new UpdateNicknameResponseDto(requestDto.getNickName());
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
-    @PutMapping("/{userId}/password")
-    public ResponseEntity<String> updatePassword(@PathVariable Long userId,
-                                                 @RequestBody UpdatePasswordRequestDto request) {
-        profileService.updateUserPassword(userId, request);
-        return new ResponseEntity<>("비밀번호 수정이 완료되었습니다.", HttpStatus.OK);
+    // 비밀번호 수정
+    @PutMapping("/password")
+    public ResponseEntity<UpdatePasswordResponseDto> updatePassword(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                                    @RequestBody @Valid UpdatePasswordRequestDto requestDto) {
+        Long userId = userDetails.getUser().getId();
+        profileService.updateUserPassword(userId, requestDto);
+        UpdatePasswordResponseDto responseDto = new UpdatePasswordResponseDto("비밀번호가 성공적으로 변경되었습니다.");
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
